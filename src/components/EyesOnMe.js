d0b1014509521bac2d901be612cb6c1a886587da
kelly r maass <kmaass77627@gmail.com>
2021-02-18 18:02:05 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+
+  render() {
+    return(
+
+      <button></button>
+    )
+  }
+}
